dc3348c12b0103d0f546dc8b75c48795acf811b6
xu.pengfei <1508075252@qq.com>
2022-12-17 10:23:19 +0800

    prettier

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,20 +1,17 @@
 import path from 'node:path';
 import { defineConfig } from 'vite';
 
-
 export default defineConfig({
-    plugins: [
-    ],
-    build: {
-        sourcemap: true,
-        cssCodeSplit: true,
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            name: 'FaComponents',
-            formats: ['es', 'umd'],
-            fileName: (format) => `fa-components.${format}.js`,
-        },
-        rollupOptions: {
-        },
+  plugins: [],
+  build: {
+    sourcemap: true,
+    cssCodeSplit: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'FaComponents',
+      formats: ['es', 'umd'],
+      fileName: (format) => `fa-components.${format}.js`,
     },
+    rollupOptions: {},
+  },
 });
